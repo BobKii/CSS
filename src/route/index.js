0db299250409ad669f014f78d7e3392f6a618c1d
BobKii <two.heap2008@gmail.com>
2023-09-13 20:11:32 +0300

    130923 point_day_1 CSS template_DZ_Slack

--- src/route/index.js
+++ src/route/index.js
@@ -1153,7 +1153,7 @@ router.get("/slack-dz", function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render("slack", {
+  res.render("slack-dz", {
     layout: null,
   });
 });
